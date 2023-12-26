d2f08f87a5cebc1979b0ac64dacecec7c1cf4693
Claudio Carige <ccarige@gmail.com>
2023-12-26 14:25:53 -0300

    Changed module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/components-home/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/components-home/header/header.component';
+import { SideMenuComponent } from './components/components-home/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
